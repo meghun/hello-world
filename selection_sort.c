884d1fdd1e4af66648c6d87ffeb6dce3d419ccb6
megiris <bmegan5@vt.edu>
2018-01-24 20:11:35 -0500

    working sort without pointers

---
+++ selection_sort.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int numbers[10] = {23, 4, 5, 67, 11, 30, 98, -4, -28, 10}; 
+
+
+
+int main()
+{
+
+int len = 10;
+int i, j; 
+int min_loc; 
+int x, y, temp;
+
+
+for (i = 0; i < len-1; i++) 
+{
+
+min_loc = i; //Sets minimum to null 
+
+//j will look at element next to current minimum and compare the value at that array element
+for (j= i+1; j< len; j++) 
+	if (numbers[j] < numbers[min_loc]) 
+		min_loc = j;
+
+	temp = numbers[min_loc]; 
+	y = numbers[i];
+	numbers[i] = temp; 
+	numbers[min_loc] = y; 
+
+}
+
+for (i = 0; i < len; i ++) 
+	{
+	printf ("%d, ", numbers[i]);
+	}
+
+
+int max; 
+max = numbers[9];
+printf("The max number is: %d \n", max);
+
+return 0;
+}
+
+
+
+
+
